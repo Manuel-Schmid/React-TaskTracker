62e5a103044362c415d7085f7ae6db4a9b22dcc8
Manuel Schmid <manysch3@gmail.com>
2021-09-25 23:01:46 +0200

    changed text casing

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,10 +2,10 @@ import {FaTimes} from "react-icons/fa";
 
 const Task = ({task, onDelete, onToggle}) => {
     return (
-        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}> {/* if the reminder is true, add the class 'reminder'*/}
+        <div className={`task ${task.reminder ? 'Reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}> {/* if the reminder is true, add the class 'reminder'*/}
             <h3>
                 {task.text}
-                <FaTimes style={{ color: 'red', cursor: 'pointer'}} onClick={() => onDelete(task.id)}/>
+                <FaTimes style={{ color: 'red', cursor: 'Pointer'}} onClick={() => onDelete(task.id)}/>
             </h3>
             <p>{task.day}</p>
         </div>
