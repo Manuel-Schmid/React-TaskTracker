c6962b9af6bbd38d0d6c4652eb7c6e84a9e79001
Manuel Schmid <manysch3@gmail.com>
2021-09-26 18:26:36 +0200

    updating version

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const About = () => {
     return (
         <div>
-            <h4>Version 1.2.3</h4>
+            <h4>Version 1.2.4</h4>
             <Link to='/'>Go Back</Link> {/* <Link> doesn't reload the page while <a> does */}
         </div>
     )
